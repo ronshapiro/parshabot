c777920ef7e1e2b22209df7a15decf2173bdad19
Ron Shapiro <ronshapiro@google.com>
2019-08-25 13:53:35 +0300

    Basic timezone support

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-var Twit = require('twit');
-var fs = require('fs');
 var sedra = require("./sedra.js");
 var initializeTwitter = require("./initializeTwitter.js");
+var {DateTime, IANAZone} = require("luxon");
+
+var timezones = {
+  israel: IANAZone.create("Asia/Jerusalem"),
+  nyc: IANAZone.create("America/New_York"),
+}
+
+function dayOfWeekAsInt(timezone) {
+  var asInt = DateTime.utc().setZone(timezone).weekday;
+  // Luxon treats Monday = 1, Tuesday = 2, ... Sunday = 7. Adjust so that Sunday = 1 to align
+  // roughly with aliyot
+  asInt++;
+  if (asInt === 8) {
+    asInt = 1;
+  }
+  return asInt;
+}
 
 var twitterApi = initializeTwitter();
 
 sedra.requestAliyot(function(error, aliyot) {
-  
+  console.log(aliyot[dayOfWeekAsInt(timezones.nyc).toString()]);
 });
 
 /*
@@ -14,6 +29,7 @@ sedra.requestAliyot(function(error, aliyot) {
     // console.log(data)
   })
 */  
+
 
 
 
