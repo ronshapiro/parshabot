3da1e9307a1c56daab55dd5dc96c9dcd61433083
Ron Shapiro <ronshapiro@google.com>
2019-08-25 18:08:22 +0300

    Download Hebrew as well

--- download_sefaria.sh
+++ download_sefaria.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
+
 set -eu
 
 BOOKS=(Genesis Exodus Leviticus Numbers Deuteronomy)
+
+PREFIX="https://raw.githubusercontent.com/Sefaria/Sefaria-Export/master"
 
 rm -rf sefaria-data
 
@@ -10,18 +14,45 @@ function mkdir_cd() {
 }
 
 mkdir_cd sefaria-data
+
+########################
+########################
+########################
+
 mkdir_cd schemas
 
 for book in ${BOOKS[@]}; do
-    wget -q "https://raw.githubusercontent.com/Sefaria/Sefaria-Export/master/schemas/${book}.json"
+    wget -q "${PREFIX}/schemas/${book}.json"
 done
 
 cd ..
 
+########################
+########################
+########################
+
 mkdir_cd books
 
 for book in ${BOOKS[@]}; do
-    wget -q -O ${book}.json "https://raw.githubusercontent.com/Sefaria/Sefaria-Export/master/json/Tanakh/Torah/${book}/English/The%20Koren%20Jerusalem%20Bible.json"
+    wget -q -O ${book}.json "${PREFIX}/json/Tanakh/Torah/${book}/English/The%20Koren%20Jerusalem%20Bible.json"
 done
 
-cd ../..
+cd ..
+
+########################
+########################
+########################
+
+mkdir_cd books-he
+
+for book in ${BOOKS[@]}; do
+    wget -q -O ${book}.json "${PREFIX}/json/Tanakh/Torah/${book}/Hebrew/Tanach%20with%20Ta'amei%20Hamikra.json"
+done
+
+cd ..
+
+########################
+########################
+########################
+
+cd ..
